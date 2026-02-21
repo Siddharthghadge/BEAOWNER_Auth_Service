89c2df6845486f699dc704d110bd834af320be8c
ADMIN <siddharthghadge.skncoe.mech@gmail.com>
2026-02-21 14:01:56 +0530

    commit

--- src/main/java/com/carrental/authservice/client/UserServiceClient.java
+++ src/main/java/com/carrental/authservice/client/UserServiceClient.java
@@ -5,7 +5,7 @@ import com.carrental.authservice.dto.UserResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(name = "userservice", url = "${USER_SERVICE_URL}")
+@FeignClient(name = "userservice", url = "${user.service.url}")
 public interface UserServiceClient {
 
     @PostMapping("/api/users/register/internal")
